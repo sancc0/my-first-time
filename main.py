7a7db6747731a5379f9838b79b7085bbd0f90082
“sancc0” <189404197+sancc0@users.noreply.github.com>
2024-12-03 08:00:31 -0500

    Prueba de codigo

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("Hello World")
+# VARIABLES
+name = "Ian Contreras"
+age = 16
+heigth = 1.68
+student = True
+# CONJUNTO DE DATOS
+hobbies = list ()
+hobbies = ["Dormir", "Jugar", "Comer", "Comprar Ropa"]
+#hobbies[4] = "Gym"
+
+dato = input("ingrese un nuevo hobbie: ")
+hobbies.append(dato)
+
+print(hobbies)
